1ca44cb5b34def5142579773fabd812316f6f187
Dylan Hawkes <dthawkes@google.com>
2023-02-14 20:06:01 +0000

    Added some comments and sections to the commandline arguments
    Signed-off-by: Dylan Hawkes <dthawkes@google.com>

--- src/flags.cc
+++ src/flags.cc
@@ -4,11 +4,16 @@
 
 #include "absl/flags/flag.h"
 
+// General Options
+ABSL_FLAG(uint32_t, sat_runtime, 20,
+          "The desired duration of the stress test, in seconds.");
+
 ABSL_FLAG(
     bool, sat_use_coarse_grain_queues, false,
     "Whether to use coarse or fine grain lock queues during testing. By "
     "default fine grain lock queues will be used as they are more efficient.");
 
+// Memory Copy Test
 ABSL_FLAG(
     uint64_t, sat_memory, 0,
     "The amount of RAM to test in Megabytes. A value of 0 (the "
@@ -24,21 +29,21 @@ ABSL_FLAG(uint64_t, sat_reserve_memory, 0,
 ABSL_FLAG(uint64_t, sat_hugepage_memory, 0,
           "The minimum amount of hugepage RAM to test in Megabytes.");
 
-ABSL_FLAG(uint32_t, sat_runtime, 20,
-          "The desired duration of the stress test, in seconds.");
-
 ABSL_FLAG(int32_t, sat_memory_threads, -1,
           "The number of memory copy threads to run. By default, this will "
           "equal the number of CPU cores.");
 
+// Memory Invert Test
 ABSL_FLAG(
     uint32_t, sat_invert_threads, 0,
     "The number of memory invert threads to run. None will be run by default.");
 
+// Memory Check Test
 ABSL_FLAG(
     uint32_t, sat_check_threads, 0,
     "The number of memory check threads to run. None will be run by default.");
 
+// CPU Cache Coherence Test
 ABSL_FLAG(bool, sat_test_cache_coherence, false,
           "Whether to run the CPU Cache Coherence test, which verifies the "
           "CPU cache by incrementing counters from threads running on "
@@ -57,6 +62,7 @@ ABSL_FLAG(uint32_t, sat_cache_line_count, 2,
           "The amount of cache-line-sized data structures to use for the cache "
           "coherence test. The default is 2.");
 
+// CPU Frequency Test
 ABSL_FLAG(bool, sat_test_cpu_frequency, false,
           "Whether to run the CPU frequency test, which validates the CPU "
           "frequency against a user specified threshold.");
--- src/flags.h
+++ src/flags.h
@@ -2,18 +2,29 @@
 
 #include "absl/flags/declare.h"
 
+// General Options
+ABSL_DECLARE_FLAG(uint32_t, sat_runtime);
 ABSL_DECLARE_FLAG(bool, sat_use_coarse_grain_queues);
+
+// Memory Copy Test
 ABSL_DECLARE_FLAG(uint64_t, sat_memory);
 ABSL_DECLARE_FLAG(uint64_t, sat_reserve_memory);
 ABSL_DECLARE_FLAG(uint64_t, sat_hugepage_memory);
-ABSL_DECLARE_FLAG(uint32_t, sat_runtime);
 ABSL_DECLARE_FLAG(int32_t, sat_memory_threads);
+
+// Memory Invert Test
 ABSL_DECLARE_FLAG(uint32_t, sat_invert_threads);
+
+// Memory Check Test
 ABSL_DECLARE_FLAG(uint32_t, sat_check_threads);
+
+// CPU Cache Coherence Test
 ABSL_DECLARE_FLAG(bool, sat_test_cache_coherence);
 ABSL_DECLARE_FLAG(uint32_t, sat_cache_increment_count);
 ABSL_DECLARE_FLAG(uint32_t, sat_cache_line_size);
 ABSL_DECLARE_FLAG(uint32_t, sat_cache_line_count);
+
+// CPU Frequency Test
 ABSL_DECLARE_FLAG(bool, sat_test_cpu_frequency);
 ABSL_DECLARE_FLAG(uint32_t, sat_cpu_frequency_threshold);
 ABSL_DECLARE_FLAG(uint32_t, sat_cpu_frequency_round);
